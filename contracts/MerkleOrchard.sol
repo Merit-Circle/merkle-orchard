2d1e6ba7ccdce2dc663a11aaa1f24b533e997743
Fede Rava <federrava@gmail.com>
2022-06-06 15:22:43 +0200

    QSP-11 (#10)
    * Added events
    
    claim, setMerkleRoot, fundChannelWithETH, fundChannel
    
    * Added event tests
    
    All events work fine
    
    * prettier
    
    Co-authored-by: Mick de Graaf <mick@mickdegraaf.nl>

--- contracts/MerkleOrchard.sol
+++ contracts/MerkleOrchard.sol
@@ -18,6 +18,11 @@ contract MerkleOrchard is ERC721Enumerable, IMerkleOrchard {
     error NotOwnerError();
     error NonExistentTokenError();
 
+    event ChannelFunded(uint256 indexed channelId, address indexed token);
+    event ChannelFundedWithETH(uint256 indexed channelId);
+    event MerkleRootSet(uint256 indexed channelId, bytes32 indexed merkleRoot);
+    event TokenClaimed(uint256 indexed channelId, address indexed receiver, address indexed token);
+
     struct Channel {
         mapping(address => uint256) reserves;
         // token => account => amount
@@ -51,6 +56,7 @@ contract MerkleOrchard is ERC721Enumerable, IMerkleOrchard {
 
         IERC20(_token).safeTransferFrom(msg.sender, address(this), _amount);
         channels[_channelId].reserves[_token] += _amount;
+        emit ChannelFunded(_channelId, _token);
     }
 
     function getChannelReservesByToken(uint256 _channelId, address _token) public view returns (uint256) {
@@ -63,6 +69,7 @@ contract MerkleOrchard is ERC721Enumerable, IMerkleOrchard {
         }
 
         channels[_channelId].reserves[address(0)] += msg.value;
+        emit ChannelFundedWithETH(_channelId);
     }
 
     function setMerkleRoot(uint256 _channelId, bytes32 _merkleRoot) external {
@@ -71,6 +78,7 @@ contract MerkleOrchard is ERC721Enumerable, IMerkleOrchard {
         }
 
         channels[_channelId].merkleRoot = _merkleRoot;
+        emit MerkleRootSet(_channelId, _merkleRoot);
     }
 
     function getMerkleRoot(uint256 _channelId) public view returns (bytes32) {
@@ -104,9 +112,10 @@ contract MerkleOrchard is ERC721Enumerable, IMerkleOrchard {
         if (_token == address(0)) {
             // solhint-disable-next-line
             payable(_receiver).call{ value: withdrawAmount }("");
-            return;
+        } else {
+            IERC20(_token).safeTransfer(_receiver, withdrawAmount);
         }
-        IERC20(_token).safeTransfer(_receiver, withdrawAmount);
+        emit TokenClaimed(_channelId, _receiver, _token);
     }
 
     function _baseURI() internal view virtual override returns (string memory) {
